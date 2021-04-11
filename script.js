728aa231736260544184ee03cf8bfeedc69b4eab
AswanthPalani <81860467+AswanthPalani@users.noreply.github.com>
2021-04-11 15:32:11 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function changeBulbImage1() {
+	document.getElementById("bulbImg").src = "light_bulb_off.png";
+}
+function changeBulbImage2() {
+	document.getElementById("bulbImg").src = "light_bulb_on.png";
+}
